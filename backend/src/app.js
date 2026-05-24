d987a45aabd6396e1a5737b13ee5055f89bd33d6
Shinde Pandurang <ps6308339@gmail.com>
2026-05-24 07:03:04 +0530

    Backend ready for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,39 +1,34 @@
 import express from "express";
-import { createServer } from "node:http";
-
+import {createServer} from "http";
 import { Server } from "socket.io";
-
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
-
 import cors from "cors";
-import userRoutes from "./routes/users.routes.js";
+import userRoutes from "./routers/user.router.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
 
-app.set("port", (process.env.PORT || 8000))
-app.use(cors());
-app.use(express.json({ limit: "40kb" }));
-app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.set("PORT", process.env.PORT || 8080); 
 
-app.use("/api/v1/users", userRoutes);
+app.use(cors());
+app.use(express.urlencoded({limit:"40kb", extended:true}));
+app.use(express.json({limit:"40kb"}))
+
+app.use("/api/user",userRoutes);
 
 const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb://localhost:27017/zoom_main")
-
-    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
+    app.set("MONGO_USER");
+    const connectionDb = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MONGO CONNECTED DB HOST ${connectionDb.connection.host}`)
+    server.listen(app.get("PORT"), () => {
+      console.log(`LISTENING ON PORT ${app.get("PORT")}`);
     });
 
-
-
-}
-
-
+};
 
 start();
